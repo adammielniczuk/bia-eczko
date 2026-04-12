e4f09d163bf04ea489b5fae1b829fae3526d356a
MatTheTab <mateusztabaszewski@gmail.com>
2026-04-12 11:37:13 +0200

    add results and conclusions

--- project-2/run_all.py
+++ project-2/run_all.py
@@ -1,20 +1,22 @@
 import papermill as pm
 import os
 
-target_dir = "./project-2/aibi-dhi-simulator"
-notebooks = [f for f in os.listdir(target_dir) if f.endswith(".ipynb")]
+target_dir = "./aibi-dhi-simulator"
+# Get the absolute path to ensure no ambiguity
+abs_target_dir = os.path.abspath(target_dir)
+
+notebooks = [f for f in os.listdir(abs_target_dir) if f.endswith(".ipynb")]
 
 for nb in notebooks:
-    file_path = os.path.join(target_dir, nb)
-    print(f"Executing and overwriting: {nb}...")
+    file_path = os.path.join(abs_target_dir, nb)
+    print(f"Executing: {nb}...")
 
     try:
         pm.execute_notebook(
             input_path=file_path,
             output_path=file_path,
+            cwd=abs_target_dir,
         )
         print(f"Done: {nb}")
     except Exception as e:
-        print(f"Failed to run {nb}. Error: {e}")
-
-print("\nAll notebooks have been updated.")
+        print(f"Failed: {nb}. Error: {e}")
